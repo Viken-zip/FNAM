b82b17fa74547160a5aa50b488b9a8a8a362bfb2
Viken-zip <71625382+Viken-zip@users.noreply.github.com>
2023-09-02 22:03:05 +0300

    Main menu Buttons work now for runing the game in main Java Script file interval

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ export const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+export let Night = 1; //make it with localstorage soon
+export let Stars = 0; //make it with localstorage soon
+
+export const updateNight = (newValue) => Night = newValue;
+export const updateStars = (newValue) => Stars = newValue;
+
 drawMainMenu(ctx);
 
-function startGame(){
+export function startGame(Night){
     const gameInterval = setInterval(()=>{
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }, 1000/60);
+}
+
+export function endGame(interval){
+    clearInterval(interval);
+    drawMainMenu(ctx)
 }
